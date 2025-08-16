5e79eee34de41a87de23f239dde337a6592b132c
Nico Hoffmann <me@nhoffmann.com>
2025-08-16 16:22:50 +0200

    refact: Clean up `VersionsButton`

--- src/Panel/Ui/Button/VersionsButton.php
+++ src/Panel/Ui/Button/VersionsButton.php
@@ -4,6 +4,7 @@ namespace Kirby\Panel\Ui\Button;
 
 use Kirby\Cms\ModelWithContent;
 use Kirby\Content\VersionId;
+use Kirby\Http\Uri;
 use Kirby\Toolkit\I18n;
 
 /**
@@ -21,37 +22,73 @@ class VersionsButton extends ViewButton
 {
 	public function __construct(
 		ModelWithContent $model,
-		VersionId|string $versionId = 'latest'
+		public VersionId|string $versionId = 'latest'
 	) {
-		$versionId = $versionId === 'compare' ? 'compare' : VersionId::from($versionId)->value();
-		$viewUrl   = $model->panel()->url(true) . '/preview';
-
 		parent::__construct(
+			model: $model,
 			class: 'k-versions-view-button',
-			icon: $versionId === 'compare' ? 'layout-columns' : 'git-branch',
-			options: [
-				[
-					'label'   => I18n::translate('version.latest'),
-					'icon'    => 'git-branch',
-					'link'    => $viewUrl . '/latest',
-					'current' => $versionId === 'latest'
-				],
-				[
-					'label'   => I18n::translate('version.changes'),
-					'icon'    => 'git-branch',
-					'link'    => $viewUrl . '/changes',
-					'current' => $versionId === 'changes'
-				],
-				'-',
-				[
-					'label'   => I18n::translate('version.compare'),
-					'icon'    => 'layout-columns',
-					'link'    => $viewUrl . '/compare',
-					'current' => $versionId === 'compare'
-				],
-
-			],
-			text: I18n::translate('version.' . $versionId),
+			icon: $this->icon(),
+			text: I18n::translate('version.' . $this->versionId()),
 		);
+	}
+
+	public function icon(): string
+	{
+		return $this->versionId === 'compare' ? 'layout-columns' : 'git-branch';
+	}
+
+	public function isCurrent(string $versionId): bool
+	{
+		return $this->versionId() === $versionId;
+	}
+
+	public function options(): array
+	{
+		return [
+			[
+				'label'   => $this->i18n('version.latest'),
+				'icon'    => 'git-branch',
+				'link'    => $this->url('latest'),
+				'current' => $this->isCurrent('latest')
+			],
+			[
+				'label'   => $this->i18n('version.changes'),
+				'icon'    => 'git-branch',
+				'link'    => $this->url('changes'),
+				'current' => $this->isCurrent('changes')
+			],
+			'-',
+			[
+				'label'   => $this->i18n('version.compare'),
+				'icon'    => 'layout-columns',
+				'link'    => $this->url('compare'),
+				'current' => $this->isCurrent('compare')
+			],
+
+		];
+	}
+
+	public function props(): array
+	{
+		return [
+			...parent::props(),
+			'options' => $this->options()
+		];
+	}
+
+	protected function url(string $versionId): string
+	{
+		$url = $this->model->panel()->url(true) . '/preview/' . $versionId;
+		$url = new Uri($url);
+
+		return $url->toString();
+	}
+
+	public function versionId(): string
+	{
+		return match ($this->versionId) {
+			'compare' => 'compare',
+			default   => VersionId::from($this->versionId)->value()
+		};
 	}
 }
