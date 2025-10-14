bd1347668be650d95ef6da11555d991410bbd78c
Nico Hoffmann <me@nhoffmann.com>
2025-10-14 11:55:35 +0530

    refact: new `Form\Mixin\Disabled` trait

---
+++ src/Form/Mixin/Disabled.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Kirby\Form\Mixin;
+
+trait Disabled
+{
+	protected bool $disabled = false;
+
+	/**
+	 * If `true`, the field is no longer editable and will not be saved
+	 */
+	public function disabled(): bool
+	{
+		return $this->disabled;
+	}
+
+	/**
+	 * @see `self::disabled()`
+	 */
+	public function isDisabled(): bool
+	{
+		return $this->disabled();
+	}
+
+	protected function setDisabled(bool $disabled = false): void
+	{
+		$this->disabled = $disabled;
+	}
+}
